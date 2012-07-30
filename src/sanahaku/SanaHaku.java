a8c498cba92a98fec19646d6ad70f32e8019f4e5
Kati <Kati@Kati-L.P-661HNU-F1>
2012-07-30 20:05:55 +0300

    Versionhallinnan pystytys

---
+++ src/sanahaku/SanaHaku.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sanahaku;
+
+/**
+ *
+ * @author Kati
+ */
+//Kati Kyllönen 
+public class SanaHaku {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
