91771c4b7b8d6b7089b1d28e2c9ebe9e6e1b9232
Kati <Kati@Kati-L.P-661HNU-F1>
2012-08-28 22:46:35 +0300

    Apurakenteiden teko: Järjestysalgoritmin räpeltelyä

---
+++ src/apurakenteet/LisaysJarjestaminen.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apurakenteet;
+
+import sanaindeksi.TrieSolmu;
+
+/**
+ *
+ * @author Kati
+ */
+public class LisaysJarjestaminen {
+
+    public static LinkitettyLista jarjesta(LinkitettyLista jarjestettavaLista) {
+        LinkitettyLista seuraava = jarjestettavaLista.getSeuraava();
+        while (seuraava != null) {
+
+            while (jarjestettavaLista.getListaSolmu().getKirjain() > seuraava.getListaSolmu().getKirjain()) {
+                // || seuraava.getSeuraava != null)
+                if (seuraava.getSeuraava() != null) {
+                    seuraava = seuraava.getSeuraava();
+                } else {
+                    break;
+                }
+            }
+            if (jarjestettavaLista.getListaSolmu().getKirjain() > seuraava.getListaSolmu().getKirjain()) {
+                LinkitettyLista apu = jarjestettavaLista.getSeuraava();
+                jarjestettavaLista.setSeuraava(jarjestettavaLista);
+                jarjestettavaLista = apu;
+            }
+
+            seuraava = jarjestettavaLista.getSeuraava();
+        }
+        return jarjestettavaLista;
+
+    }
+}
